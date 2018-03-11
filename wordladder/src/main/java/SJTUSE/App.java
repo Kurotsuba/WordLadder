bc3337c7cd0b2ec4ef52b9614cc9f49ecd7829bd
Kurotsuba <kanneco98@gmail.com>
2018-03-11 15:56:52 +0800

    fix

--- wordladder/src/main/java/SJTUSE/App.java
+++ wordladder/src/main/java/SJTUSE/App.java
@@ -1,13 +1,10 @@
 package SJTUSE;
+import java.util.*;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
     }
 }
